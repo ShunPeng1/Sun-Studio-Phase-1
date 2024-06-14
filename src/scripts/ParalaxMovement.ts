55f47704e79f242e45a5b27a843bc7a2acd73229
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2024-06-14 08:57:01 +0700

    feat: paralax movement with y z axis

--- src/scripts/ParalaxMovement.ts
+++ src/scripts/ParalaxMovement.ts
@@ -1,16 +1,17 @@
+import { vec3 } from "gl-matrix";
 import Component from "../engine/components/Component";
 import GameObject from "../engine/scenes/GameObject";
 
 class ParalaxMovement extends Component{
     private gameObject1: GameObject;
     private gameObject2: GameObject;
-    private speed: number;
+    private speed: vec3;
 
     private maxDistance: number;
     private distanceTravelled: number = 0;
-    private defaultPosition: number;
+    private defaultPosition: vec3;
 
-    constructor(gameObject1: GameObject, gameObject2: GameObject, speed: number, maxDistance: number, defaultPosition : number = 0){
+    constructor(gameObject1: GameObject, gameObject2: GameObject, speed: vec3, maxDistance: number, defaultPosition : vec3 = [0,0,0]){
         super();
         this.gameObject1 = gameObject1;
         this.gameObject2 = gameObject2;
@@ -20,15 +21,19 @@ class ParalaxMovement extends Component{
     }
     
     public update(time: number, deltaTime: number){
-        this.gameObject1.transform.position[0] += this.speed * deltaTime;
-        this.gameObject2.transform.position[0] += this.speed * deltaTime;
-        this.distanceTravelled += Math.abs(this.speed * deltaTime);
+        let deltaPosition = vec3.create();
+        vec3.scale(deltaPosition, this.speed, deltaTime);
+        vec3.add(this.gameObject1.transform.position, this.gameObject1.transform.position, deltaPosition);
+        vec3.add(this.gameObject2.transform.position, this.gameObject2.transform.position, deltaPosition);
+
+        this.distanceTravelled += Math.abs(vec3.length(deltaPosition));
 
         if (this.distanceTravelled >= this.maxDistance){
 
             this.distanceTravelled = 0;
 
-            this.gameObject2.transform.position[0] = this.defaultPosition;
+            // Reset the position of gameObject1
+            vec3.copy(this.gameObject1.transform.position, this.defaultPosition);
 
             // Swap the positions of gameObject1 and gameObject2
             let tempPosition = this.gameObject1.transform.position[0];
