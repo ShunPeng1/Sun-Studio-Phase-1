0ccfa3b90cf659312af8a9c4439a5febe310d459
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2024-06-14 15:12:34 +0700

    feat: platform destroyer

---
+++ src/scripts/platforms/PlatformDestroyer.ts
@@ -0,0 +1,25 @@
+import Collider from "../../engine/components/Collider";
+import Component from "../../engine/components/Component";
+import Platform from "./Platform";
+
+class PlatformDestroyer extends Component{
+    public clone(): Component {
+        return new PlatformDestroyer();
+    }
+
+    public awake(): void {
+        let collider = this.gameObject.getComponent<Collider>(Collider)!;
+        
+        collider.subcribeToCollisionEnter(this.destroyPlatform.bind(this));
+        collider.subcribeToCollisionStay(this.destroyPlatform.bind(this));
+
+    }
+
+    private destroyPlatform(other : Collider) : void {
+        if (other.gameObject.getComponent<Platform>(Platform)) {
+            other.gameObject.destroy();
+        }
+    }
+}
+
+export default PlatformDestroyer;
