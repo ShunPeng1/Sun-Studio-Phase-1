a2c0f7eee400741723253b67b20373224df131d1
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2024-06-17 09:52:50 +0700

    fix: jittering movementw

--- src/engine/components/Rigidbody.ts
+++ src/engine/components/Rigidbody.ts
@@ -25,6 +25,9 @@ class Rigidbody extends Component {
 
         if (this.isKinematic) return;
 
+        // Apply gravity
+        this.addForce([0, -this.gravity, 0]);
+        
         let deltaVelocity = vec3.create();
         vec3.scale(deltaVelocity, this.acceleration, fixedDeltaTime);
         vec3.add(this.velocity, this.velocity, deltaVelocity);
@@ -32,16 +35,14 @@ class Rigidbody extends Component {
         // Update position
         let deltaPositionByVelocity = vec3.create(), deltaPositionByAccerelation = vec3.create();
         vec3.scale(deltaPositionByVelocity, this.velocity, fixedDeltaTime);        
-        vec3.scale(deltaPositionByAccerelation, this.acceleration, 0.5 * fixedDeltaTime * fixedDeltaTime);
+        //vec3.scale(deltaPositionByAccerelation, this.acceleration, 0.5 * fixedDeltaTime * fixedDeltaTime);
         vec3.add(this.gameObject.transform.position, this.gameObject.transform.position, deltaPositionByVelocity);
-        vec3.add(this.gameObject.transform.position, this.gameObject.transform.position, deltaPositionByAccerelation);
+        //vec3.add(this.gameObject.transform.position, this.gameObject.transform.position, deltaPositionByAccerelation);
         
         
         // Reset acceleration for the next time step
         vec3.set(this.acceleration, 0, 0, 0);
 
-        // Apply gravity
-        this.addForce([0, -this.gravity, 0]);
 
 
 
